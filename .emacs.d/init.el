638546298023f906c7879c5679518c346b833f07
Katsuki Kobayashi <rare@tirasweel.org>
2019-04-14 20:09:09 +0900

    use rls instead of racer

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -43,9 +43,13 @@
                     (add-hook 'before-save-hook
                               'clang-format-buffer))))
 
-;; company-racer (for Rust)
-(with-eval-after-load 'company
-  (add-to-list 'company-backends 'company-racer))
+;; Rust
+;(add-hook 'rust-mode-hook (lambda ()
+;                            (racer-mode)
+;                            (flycheck-rust-setup)))
+(eval-after-load "rust-mode"
+  '(setq-default rust-format-on-save t))
+(add-hook 'rust-mode-hook 'lsp)
 
 ;; git-gutter+
 (global-git-gutter+-mode)
@@ -133,24 +137,6 @@
 ;; YaTeX-mode
 (load "yatex")
 (add-to-list 'auto-mode-alist '("\\.tex" . yatex-mode))
-
-;; rust-mode
-;; see <http://keens.github.io/blog/2016/12/29/kizuitararustnokankyoukouchikugakanarirakuninatteta/>
-;;; rust-modeでrust-format-on-saveをtにすると自動でrustfmtが走る
-(eval-after-load "rust-mode"
-  '(setq-default rust-format-on-save t))
-;;; rustのファイルを編集するときにracerとflycheckを起動する
-(add-hook 'rust-mode-hook (lambda ()
-                            (racer-mode)
-                            (flycheck-rust-setup)))
-;;; racerのeldocサポートを使う
-(add-hook 'racer-mode-hook #'eldoc-mode)
-;;; racerの補完サポートを使う
-(add-hook 'racer-mode-hook (lambda ()
-                             (company-mode)
-                             ;;; この辺の設定はお好みで
-                             (set (make-variable-buffer-local 'company-idle-delay) 0.1)
-                             (set (make-variable-buffer-local 'company-minimum-prefix-length) 3)))
 
 ;; golang
 (add-hook 'before-save-hook #'gofmt-before-save)
