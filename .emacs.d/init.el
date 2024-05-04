a5d92d9e3b58dc81c450e141d8aea4f0229916a2
Katsuki Kobayashi <rare@tirasweel.org>
2024-05-04 22:34:36 +0900

    reformatterのパッケージのインストールとbiome用の設定を追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,6 +25,7 @@
 (straight-use-package 'org-mode) ;; TODO: Windowsのみ無効にしたい? makeが通らない.
 (straight-use-package 'org-journal)
 (straight-use-package 'powerline)
+(straight-use-package 'reformatter)
 (straight-use-package 'rustic)
 (straight-use-package 'swift-mode)
 (straight-use-package 'web-mode)
@@ -181,6 +182,12 @@ redrawが non-nilの場合は、Windowを再描画します。"
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
+
+;; biome <https://zenn.dev/craneduck/articles/3c0da51d4767d5>
+(reformatter-define biome-format ;; 第1引数はわかりやすい自由な名前
+  :program "biome" ;; コマンド。PATHが通っていること(後述のadd-node-modules-pathを使っても良い)
+  :args `("format" "--stdin-file-path" ,(buffer-file-name)) ;; オプション引数のリスト、実行時に評価される
+  :lighter " BiomeFmt") ;; minor-modeとして使う場合の、モードラインに表示する名前(先頭にスペースが必要)
 
 ;; Cでのインデントをいじる
 (add-hook 'c-mode-hook
