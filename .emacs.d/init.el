165a47f87cd6d18c875b197032e2662649bae06d
Katsuki Kobayashi <rare@tirasweel.org>
2025-05-08 23:46:27 +0900

    .emacsを再構成

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,12 @@
+;; キーバインド
+(global-unset-key (kbd "C-z"))
+(global-set-key (kbd "<f5>") 'revert-buffer)   ; f5 - revert buffer
+(global-set-key (kbd "<f11>") 'toggle-frame-fullscreen)        ; f11 - full screen
+(global-set-key "\C-h" 'backward-delete-char)  ; C-h should be backspace!
+(global-set-key "\C-j" nil)                    ; C-j
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key "\C-x\C-f" 'counsel-find-file)
+
 ;; straight
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -14,74 +23,62 @@
 
 (setq package-enable-at-startup nil)
 
-;; パッケージのインストール
-(straight-use-package 'company-mode)
-(straight-use-package 'counsel)
-(straight-use-package 'ddskk)
-(straight-use-package 'elscreen)
-(straight-use-package 'kotlin-mode)
-(straight-use-package 'lsp-mode)
-(straight-use-package 'moe-theme)
-(straight-use-package 'org-mode) ;; TODO: Windowsのみ無効にしたい? makeが通らない.
-(straight-use-package 'org-journal)
-(straight-use-package 'powerline)
-(straight-use-package 'reformatter)
-(straight-use-package 'rustic)
-(straight-use-package 'swift-mode)
-(straight-use-package 'web-mode)
-(straight-use-package 'yasnippet)
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+(use-package eglot)
+(add-to-list 'eglot-server-programs
+	     '(python-mode . ("pyright-langserver" "--stdio")))
+(use-package python-mode
+  :ensure nil
+  :hook
+  (python-mode . eglot-ensure))
+
+(use-package company
+  :ensure t
+  :init
+  (global-company-mode)
+  :config
+  (setq company-idle-delay 0.2
+        company-minimum-prefix-length 1
+        company-tooltip-align-annotations t
+        company-backends '(company-capf))
+  :bind
+  (:map company-active-map
+	      ("C-n". company-select-next)
+	      ("C-p". company-select-previous)
+	      ("M-<". company-select-first)
+	      ("M->". company-select-last))
+  )
+
+(use-package elscreen
+  :config
+  (setq elscreen-prefix-key (kbd "C-z"))
+  :init
+  (elscreen-start)
+  )
+
+(use-package counsel)
+
+(use-package powerline)
+(use-package moe-theme
+  :config
+  (moe-dark)
+  (powerline-moe-theme)
+  (setq moe-theme-select-color 'cyan)
+  (set-face-foreground 'font-lock-comment-face "pink")
+  )
+
 
 
 ;; バックアプファイルを無効
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
-;; キーバインド
-(global-set-key (kbd "<f5>") 'revert-buffer)   ; f5 - revert buffer
-(global-set-key (kbd "<f11>") 'toggle-frame-fullscreen)        ; f11 - full screen
-(global-set-key "\C-h" 'backward-delete-char)  ; C-h should be backspace!
-(global-set-key "\C-j" nil)                    ; C-j
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key "\C-x\C-f" 'counsel-find-file)
-
-
-;; moe-theme & powerline
-(require 'powerline)
-(require 'moe-theme)
-(moe-dark)
-(powerline-moe-theme)
-(setq moe-theme-select-color 'cyan)
-(set-face-foreground 'font-lock-comment-face "pink")
-;(set-face-foreground 'whitespace-tab "darkblue")
-;(set-face-underline  'whitespace-tab t)
-;(set-face-background 'whitespace-tab nil)
-
 ;; 表示設定
 (menu-bar-mode 0)
 (tool-bar-mode 0)
-;(scroll-bar-mode 0)
-
-
-;; 言語設定
-(set-language-environment "Japanese")
-(prefer-coding-system 'utf-8)
-(setq default-input-method "japanese-skk")
-
-;; elscreen
-(global-unset-key (kbd "C-z"))
-(load "elscreen")
-(setq elscreen-prefix-key (kbd "C-z"))
-(elscreen-start)
-
-;; rustic
-(setq rustic-format-trigger 'on-save)
-
-;; yasnippet
-(yas-global-mode)
-
-;; 行番号表示.
-(require 'linum)
-(global-linum-mode)
+(scroll-bar-mode 0)
 
 ;; 括弧を表示.
 (show-paren-mode 1)
@@ -95,46 +92,6 @@
 ;; 折り返さない
 (setq truncate-lines t)
 
-;; タブ幅変更関数
-(defun set-aurora-tab-width (num &optional local redraw)
-  "タブ幅をセットします。タブ5とかタブ20も設定できたりします。
-localが non-nilの場合は、カレントバッファでのみ有効になります。
-redrawが non-nilの場合は、Windowを再描画します。"
-  (interactive "nTab Width: ")
-  (when local
-    (make-local-variable 'tab-width)
-    (make-local-variable 'tab-stop-list))
-  (setq tab-width num)
-  (setq c-basic-offset num)
-  (setq tab-stop-list ())
-  (while (<= num 256)
-    (setq tab-stop-list `(,@tab-stop-list ,num))
-    (setq num (+ num tab-width)))
-  (when redraw (redraw-display)) tab-width)
-
-(define-key global-map "\C-c\C-t" 'set-aurora-tab-width)
-
-;; 無駄な文字のハイライト
-(setq-default show-trailing-whitespace t)
-;(setq-default whitespace-space-regexp "\x3000+")
-(setq-default whitespace-style
-	      '(face
-		tabs
-		;; spaces
-		trailing
-		;; lines
-		space-before-tab
-		newline
-		;; indentation
-		empty
-		space-after-tab
-		;; big-indent
-		;; space-mark
-		;; tab-mark
-		))
-(setq-default whitespace-line-column 160)
-(global-whitespace-mode 1)
-
 ;; make C-a lovely
 (define-key global-map "\C-a"
 #'(lambda (arg)
@@ -143,92 +100,5 @@ redrawが non-nilの場合は、Windowを再描画します。"
 	  (back-to-indentation)
 	(beginning-of-line arg))))
 
-; Org-captureの設定
-; Org-captureを呼び出すキーシーケンス
-(define-key global-map "\C-cc" 'org-capture)
-; Org-captureのテンプレート（メニュー）の設定
-(setq org-capture-templates
-      '(("n" "Note" entry (file+headline "~/Org/notes.org" "Notes")
-         "* %?\nEntered on %U\n %i\n %a")
-        ))
 
-(defun show-org-buffer (file)
-  "Show an org-file FILE on the current buffer."
-  (interactive)
-  (if (get-buffer file)
-      (let ((buffer (get-buffer file)))
-        (switch-to-buffer buffer)
-        (message "%s" file))
-    (find-file (concat "~/Org/" file))))
-(global-set-key (kbd "C-M-^") '(lambda () (interactive)
-                                 (show-org-buffer "notes.org")))
-
-(setq org-agenda-files (list "~/Org"
-                             "~/Org/journal"))
-(setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
-
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "SOMEDAY(s)" "PENDING(p)" "WAITING(w)" "|" "DONE(d)" "GIVEUP(u)")))
-
-(setq org-journal-dir "~/Org/journal")
-(setq org-journal-date-format "%Y %m/%d %A")
-(setq org-journal-time-format "%m/%d %R")
-
-(setq org-journal-carryover-items
-      "TODO=\"TODO\"|TODO=\"PENDING\"|TODO=\"SOMEDAY\"|TODO=\"WAITING\"")
-(setq org-journal-file-format "%Y%m%d.org")
-
-;; web-mode
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tsx?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
-
-;; biome <https://zenn.dev/craneduck/articles/3c0da51d4767d5>
-(reformatter-define biome-format ;; 第1引数はわかりやすい自由な名前
-  :program "npx" ;; コマンド。PATHが通っていること(後述のadd-node-modules-pathを使っても良い)
-  :args `("biome" "format" "--stdin-file-path" ,(buffer-file-name)) ;; オプション引数のリスト、実行時に評価される
-  :lighter " BiomeFmt") ;; minor-modeとして使う場合の、モードラインに表示する名前(先頭にスペースが必要)
-
-(reformatter-define biome-check
-  :program "npx"
-  :args `("biome" "check" "--apply-unsafe" "--stdin-file-path" ,(buffer-file-name))
-  :lighter " BiomeCheck")
-
-(reformatter-define ruff-format
-  :program "uv"
-  :args `("run" "ruff" "format" "--stdin-filename" ,(or (buffer-file-name) input-file))
-  :lighter " RuffFmt")
-
-;; Cでのインデントをいじる
-(add-hook 'c-mode-hook
-  '(lambda ()
-     (c-set-offset 'extern-lang-open 0)
-     (c-set-offset 'extern-lang-close 0)
-     (c-set-offset 'inextern-lang 0))
-  )
-
-;; start emacs-server
-(server-start)
-
-;; Windowsのみにしたい
-;(custom-set-faces
-; ;; custom-set-faces was added by Custom.
-; ;; If you edit it by hand, you could mess it up, so be careful.
-; ;; Your init file should contain only one such instance.
-; ;; If there is more than one, they won't work right.
-; '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 90 :width normal)))))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((eval add-hook 'before-save-hook #'biome-check-buffer nil t)
-     (eval add-hook 'before-save-hook #'biome-format-buffer nil t))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(global-display-line-numbers-mode 1)
